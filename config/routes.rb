6dcbd0b080fca305c6173d50b1325cc94fe8eb3f
khris <khris56@hotmail.com>
2018-10-11 17:18:29 -0500

    cambia style

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
  post '/posts', to: 'posts#create'
  get '/posts/:id', to: 'posts#show', as: 'post'
  get '/posts/:id/edit', to: 'posts#edit'
+ patch '/posts/:id', to: 'posts#update'
 end
