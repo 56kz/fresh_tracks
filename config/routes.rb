f9d1569c82cee164e26ea73b09f3c7c6b6d48c87
khris <khris56@hotmail.com>
2018-10-16 17:06:08 -0500

    Autentiación de usuarios completada

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users, only: [:new, :create]
   resources :posts
   # get '/posts', to: 'posts#index'
