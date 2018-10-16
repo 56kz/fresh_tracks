6c43143030fb32ebff284ff5c4f1bba5fa345118
khris <khris56@hotmail.com>
2018-10-16 16:35:46 -0500

    Implementa funcionalidad d registar usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
- # get '/posts', to: 'posts#index'
- # get '/posts/new', to: 'posts#new', as: 'new_post'
- # post '/posts', to: 'posts#create'
- # get '/posts/:id', to: 'posts#show', as: 'post'
- # get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
- # patch '/posts/:id', to: 'posts#update'
- # delete '/posts/:id', to: 'posts#destroy'
- resources :posts
- root 'posts#index'
+  resources :users, only: [:new, :create]
+
+  resources :posts
+  root 'posts#index'
+  # get '/posts', to: 'posts#index'
+  # get '/posts/new', to: 'posts#new', as: 'new_post'
+  # post '/posts', to: 'posts#create'
+  # get '/posts/:id', to: 'posts#show', as: 'post'
+  # get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
+  # patch '/posts/:id', to: 'posts#update'
+  # delete '/posts/:id', to: 'posts#destroy'
 end
